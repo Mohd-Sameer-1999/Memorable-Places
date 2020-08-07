8b57f936d32e690dd9ffe787bb6b01232b715bef
Mohd-Sameer <mohdsameer2006@gmail.com>
2020-08-07 17:44:48 +0530

    Initial commit

---
+++ app/src/main/java/com/mycompany/sameer/memorableplaces/MapsActivity.java
@@ -0,0 +1,175 @@
+package com.mycompany.sameer.memorableplaces;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void centreOnMapLocation(Location location, String title) {
+        if (location != null) {
+            LatLng usersLocation = new LatLng(location.getLatitude(), location.getLongitude());
+//            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(usersLocation).title(title));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(usersLocation, 12));
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
+
+        Intent intent = getIntent();
+        if(intent.getIntExtra("placeNumber", 0) == 0) {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    centreOnMapLocation(location, "Your Location");
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+            }else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location  lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centreOnMapLocation(lastKnownLocation, "Your Location");
+            }
+        }else{
+            Location thatLocation = new Location(LocationManager.GPS_PROVIDER);
+            thatLocation.setLatitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).latitude);
+            thatLocation.setLongitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).longitude);
+
+            centreOnMapLocation(thatLocation, MainActivity.memorablePlace.get(intent.getIntExtra("placeNumber", 0)));
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location  lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                mMap.clear();
+                centreOnMapLocation(lastKnownLocation, "Your Location");
+            }
+
+        }
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(),Locale.getDefault());
+        String address = "";
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if (addressList != null && addressList.size() > 0){
+                if(addressList.get(0).getThoroughfare() != null){
+                    if(addressList.get(0).getThoroughfare() != null){
+                        address = addressList.get(0).getSubThoroughfare() + " ";
+                    }
+                    address = addressList.get(0).getThoroughfare();
+                }
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        if(address.equals("")){
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm yyyy-MM-dd");
+            address += sdf.format(new Date());
+        }
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
+        MainActivity.memorablePlace.add(address);
+        MainActivity.locations.add(latLng);
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> longitudes = new ArrayList<>();
+
+        for(LatLng coord : MainActivity.locations){
+            latitudes.add(Double.toString(coord.latitude));
+            longitudes.add(Double.toString(coord.longitude));
+        }
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences( "com.mycompany.sameer.memorableplaces", Context.MODE_PRIVATE);
+        try {
+            sharedPreferences.edit().putString("memorablePlace", ObjectSerializer.serialize(MainActivity.memorablePlace)).apply();
+            sharedPreferences.edit().putString("lat", ObjectSerializer.serialize(MainActivity.locations)).apply();
+            sharedPreferences.edit().putString("lng", ObjectSerializer.serialize(MainActivity.locations)).apply();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Toast.makeText(this, "Location saved", Toast.LENGTH_LONG).show();
+    }
+
+}
